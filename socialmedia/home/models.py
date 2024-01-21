b18d331dbdf7bb90c396ca706316ecb52d1786c8
matinsajadii <emtinsajadi79@gmail.com>
2024-01-21 23:58:18 +0330

    update project socialmedia

--- socialmedia/home/models.py
+++ socialmedia/home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -12,3 +13,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return f"{self.title} - {self.user}"
+    
+    def get_absolute_url(self):
+        return reverse("home:post_detail", args=(self.id, self.slug))
--- socialmedia/home/views.py
+++ socialmedia/home/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PostUpdateForm
 
 
 class HomeView(View):
@@ -15,5 +21,36 @@ class PostDetailView(View):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         # user = User.objects.get(pk=post)
         return render(request, "home/post_detail.html", {"post": post})
+    
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post_delete = Post.objects.get(pk=post_id)
+        if post_delete.user.id == request.user.id:
+            post_delete.delete()
+            messages.success(request, "Your Post Deleted Successfully", extra_tags="success")
+        else:
+            messages.error(request, "Your Post Wasn't Deleted", extra_tags="danger")
+    
+        return redirect("home:home")
+    
 
-# Create your views here.
+class PostUpdateView(LoginRequiredMixin, View):
+    form = PostUpdateForm
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        post_update = Post.objects.get(pk=kwargs["post_id"])
+        if not request.user.id == post_update.user.id:
+            messages.error(request, "You Cant Update This Post")
+            return redirect("home:home")
+
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form(instance=post)
+        return render(request, "home/update.html", {"form": form})
+    
+    def post(self, request):
+        pass
+
+
